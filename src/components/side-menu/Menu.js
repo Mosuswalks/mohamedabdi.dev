354732f6e6b3de94d69bd368fbc09f4be6160c82
mosuswalks <mohamedaabdi@yahoo.ca>
2020-04-01 00:00:19 -0400

    Added seperate menu components

---
+++ src/components/side-menu/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Menu } from 'antd'
+
+const Menu = () => {
+    return(
+        <Menu>
+
+        </Menu>
+    )
+}
+
+module.exports = {
+    Menu
+}
---
+++ src/components/side-menu/Sider.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Sider } from 'antd'
+import { Menu } from './Menu'
+
+
+const Sider= () => {
+
+    return(
+        <Sider>
+            <Menu>Test</Menu>
+        </Sider>
+    )
+}
+
+module.exports = {
+    Sider
+}
