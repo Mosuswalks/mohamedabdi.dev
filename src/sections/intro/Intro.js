de2778512b7ed9df7e0a261cc0d7a2974d34bb9c
mosuswalks <mohamedaabdi@yahoo.ca>
2020-04-15 21:29:42 -0400

    Fixed Typed font and size

--- src/sections/intro/Intro.js
+++ src/sections/intro/Intro.js
@@ -12,18 +12,18 @@ const Intro = () => {
   return (
     <section className={styles.container}>
       <Row>
-        <Col lg={{ offset: 4, span: 7 }}>
+        <Col lg={{ offset: 3, span: 10 }}>
           <div className={styles.typedBox}>
             <Typed
               className={styles.introText}
               strings={[
-                `Hello, I'm<br /> <span class='name-bracer-left name-brace deselect'>{</span><span class='name-text deselect'>Mohamed Abdi</span><span class='name-bracer-right deselect'>}</span><br /><span class='position-text deselect'>Full Stack Developer</span>`,
+                `Hello, I'm<br /> <span class=${styles.nameBrace}>{</span><span class=${styles.nameText}>Mohamed Abdi</span><span class=${styles.nameBrace}>}</span><br /><span class='position-text deselect'>Full Stack Developer</span>`,
               ]}
               typeSpeed={50}
             />
           </div>
         </Col>
-        <Col lg={{}}>
+        <Col lg={{ span: 8 }}>
           <Coding className={styles.coding} />
         </Col>
       </Row>
