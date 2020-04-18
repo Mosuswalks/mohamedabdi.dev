8e0d28f3293ff00ee651f9e81e0fec30d4493dcd
mosuswalks <mohamedaabdi@yahoo.ca>
2020-04-17 20:56:34 -0400

    Added Blobs to Project Sections

--- src/sections/projects/Projects.js
+++ src/sections/projects/Projects.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { Divider, Row, Col, Card, Space } from "antd"
 
+import NavyBlob from "../../images/blob-navyway.svg"
+import NavyBlob2 from "../../images/blob-navyway2.svg"
+
 import styles from "./projects.module.less"
 
 const Projects = () => {
@@ -8,7 +11,7 @@ const Projects = () => {
     <section className={styles.container}>
       <div>
         <Row>
-          <Col xs={{ offset: 2, span: 16 }}>
+          <Col xs={{ offset: 2, span: 16 }} className={styles.col}>
             <Card className={styles.cards} title="Project 1">
               lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing
               elit. Nam mauris felis, elementum in porta mollis, vestibulum at
@@ -17,20 +20,21 @@ const Projects = () => {
               tortor velit, rutrum eu augue quis, commodo tempor ante.
             </Card>
           </Col>
-          <div clasName={styles.secondStripe}>
-            <Col xs={{ offset: 6, span: 16 }} className={styles.col}>
-              <div clasName={styles.secondStripe}>
-                <Card className={styles.cards} title="Project 2">
-                  lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit. Nam mauris felis, elementum in porta mollis, vestibulum
-                  at ex. Curabitur euismod ante lorem, eu hendrerit leo semper
-                  euismod. Mauris viverra nibh dui, sed pretium mauris ultricies
-                  a. Nullam tortor velit, rutrum eu augue quis, commodo tempor
-                  ante.
-                </Card>
-              </div>
-            </Col>
-          </div>
+          <div clasName={styles.secondStripe} />
+          <NavyBlob className={styles.navyBlobOne} />
+          <Col xs={{ offset: 6, span: 16 }} className={styles.col}>
+            <div clasName={styles.secondStripe}>
+              <Card className={styles.cards} title="Project 2">
+                lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit. Nam mauris felis, elementum in porta mollis, vestibulum at
+                ex. Curabitur euismod ante lorem, eu hendrerit leo semper
+                euismod. Mauris viverra nibh dui, sed pretium mauris ultricies
+                a. Nullam tortor velit, rutrum eu augue quis, commodo tempor
+                ante.
+              </Card>
+            </div>
+          </Col>
+
           <Col xs={{ offset: 2, span: 16 }} className={styles.col}>
             <Card className={styles.cards} title="Project 3">
               lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing
@@ -40,6 +44,7 @@ const Projects = () => {
               tortor velit, rutrum eu augue quis, commodo tempor ante.
             </Card>
           </Col>
+          <NavyBlob2 className={styles.navyBlobTwo} />
         </Row>
       </div>
     </section>
