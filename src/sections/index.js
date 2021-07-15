363093c87cb9a7f57f601dc40c4301bb5188abfe
Mohamed Abdi <mohamedaabdi@yahoo.ca>
2021-07-15 11:25:40 -0400

    Initial Commit #2

--- src/sections/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Hero } from "./hero/Hero"
-export { default as About } from "./about/About"
-export { default as Projects } from "./projects/Projects"
-export { default as Contact } from "./contact/Contact"
